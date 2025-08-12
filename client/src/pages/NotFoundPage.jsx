8e140c92c70c14a2f2f03407ae70436439546fcf
DattatreyaC <dattatreya.chakraborty12@gmail.com>
2025-08-12 12:47:28 +0530

    modify 404 page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Hexagon } from "lucide-react";
 
 const NotFoundPage = () => {
     return (
         <>
             <div className="w-full h-screen flex flex-col items-center justify-center bg-radial from-white via-yellow-50 to-yellow-100">
-                <h1 className="text-5xl font-black">404</h1>
+                <div className="relative border">
+                    <Hexagon size={150} />
+                    <h1 className="text-5xl font-black absolute top-0 left-0">
+                        404
+                    </h1>
+                </div>
                 <h1 className="text-4xl font-black">OOPS!</h1>
                 <h2>Seems like the page you are looking for does not exist</h2>
             </div>
